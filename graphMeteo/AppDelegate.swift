ce1ece0468c2c2352f62495b3b8aad98866e7531
Adrhs <hamel>
2017-08-22 16:54:08 +0200

    Initial Commit

---
+++ graphMeteo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  graphMeteo
+//
+//  Created by thierryH24100 on 22/08/2017.
+//  Copyright © 2017 thierryH24100. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
