f2612bcbaaab8dcc36d3b37475a37002ffdbbd83
Adrhs <hamel>
2017-08-25 22:52:09 +0200

    ok 1

--- graphMeteo/Demo/LineChartRealTimeViewController.swift
+++ graphMeteo/Demo/LineChartRealTimeViewController.swift
@@ -27,7 +27,6 @@ class LineChartRealTimeViewController: NSViewController {
         view.window!.title = "Real Time Line"
     }
     
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
