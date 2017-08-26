42001f24c3d4d2727f33d3a50b80df2f44283c1d
thierryH91200 <thierry.hentic@gmail.com>
2017-08-26 13:26:52 +0200

    init5

---
+++ DemoChartOSX/CustomChart.swift
@@ -0,0 +1,19 @@
+//
+//  CustomChart.swift
+//  DemoChartOSX
+//
+//  Created by thierryH24100 on 26/08/2017.
+//  Copyright © 2017 thierryH24100. All rights reserved.
+//
+
+import Cocoa
+
+class CustomChart: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
